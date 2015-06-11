59c589c0f4606bb318ff6b6b739b42377cc7b1ca
Cid Donham <cdubs8600@gmail.com>
2015-06-11 11:12:44 -0700

    using lf urls for data

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ FamousEngine.init();
 
 // APP Code
 var Carousel = require('./carousel/Carousel');
-var imageData = require('./data/data');
+var imageData = require('./data/urls');
 var carousel = new Carousel('body', {pageData: imageData});
